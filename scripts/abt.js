7d6c6a6b2d21041a3be869a773f8cc97d8e67e23
user86 <kshitij.kotasthane@gmail.com>
2020-09-30 16:12:44 +0530

    Perfect

--- scripts/abt.js
+++ scripts/abt.js
@@ -4,6 +4,7 @@ sw.checked = true;
 function changeMode() {
   var modeToggle = document.getElementById("sw");
   var headers = document.getElementsByClassName("header");
+  var headerTitle = document.getElementsByClassName("header-title");
 
   if (modeToggle.checked === true) {
     // console.log("Dark Mode on");
@@ -11,7 +12,12 @@ function changeMode() {
     document.getElementsByTagName("code")[0].style =
       "background-color: white; color: black";
     for (var i = 0; i < headers.length; i++) {
-      headers[i].style = "color: white";
+      headers[i].style =
+        "color: white; font-size: 30px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
+    }
+    for (var i = 0; i < headers.length; i++) {
+      headerTitle[i].style =
+        "color: white;font-size: 50px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
     }
   } else {
     // console.log("Light Mode on");
@@ -19,7 +25,12 @@ function changeMode() {
     document.getElementsByTagName("code")[0].style =
       "background-color: black; color: white";
     for (var i = 0; i < headers.length; i++) {
-      headers[i].style = "color: black";
+      headers[i].style =
+        "color: black;font-size: 30px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
+    }
+    for (var i = 0; i < headers.length; i++) {
+      headerTitle[i].style =
+        "color: black;font-size: 50px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
     }
   }
 }
--- scripts/proj.js
+++ scripts/proj.js
@@ -4,6 +4,7 @@ sw.checked = true;
 function changeMode() {
   var modeToggle = document.getElementById("sw");
   var headers = document.getElementsByClassName("header");
+  var headerTitle = document.getElementsByClassName("header-title");
 
   if (modeToggle.checked === true) {
     // console.log("Dark Mode on");
@@ -11,11 +12,19 @@ function changeMode() {
     for (var i = 0; i < headers.length; i++) {
       headers[i].style = "color: white";
     }
+    for (var i = 0; i < headers.length; i++) {
+      headerTitle[i].style =
+        "color: white;font-size: 50px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
+    }
   } else {
     // console.log("Light Mode on");
     document.getElementsByTagName("body")[0].style = "background-color: white";
     for (var i = 0; i < headers.length; i++) {
       headers[i].style = "color: black";
+    }
+    for (var i = 0; i < headers.length; i++) {
+      headerTitle[i].style =
+        "color: black;font-size: 50px;-webkit-user-select: none; -moz-user-select: none; -ms-user-select: none; user-select: none; ";
     }
   }
 }
